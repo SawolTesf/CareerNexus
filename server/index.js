f297b340c19cade9541ddf90d38cec3c55cec33a
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2023-09-22 23:19:20 -0500

    Add middleware and Mongoose setup to index.js

--- server/index.js
+++ server/index.js
@@ -12,22 +12,39 @@ import { fileURLToPath } from "url";
 /* Config */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+// Load environment variables from .env file
 dotenv.config();
+
+// Create Express app
 const app = express();
-app.use(express.json());
-app.use(helmet());
-app.use(morgan("common"));
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app,use(cors());
-app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+// Set up middleware
+app.use(express.json()); // Parse JSON request bodies
+app.use(helmet()); // Set various HTTP headers for security
+app.use(morgan("common")); // Log HTTP requests to console
+app.use(bodyParser.json({ limit: "30mb", extended: true })); // Parse JSON request bodies with size limit
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true })); // Parse URL-encoded request bodies with size limit
+app.use(cors()); // Enable Cross-Origin Resource Sharing (CORS)
+app.use("/assets", express.static(path.join(__dirname, "public/assets"))); // Serve static assets from public/assets directory
 
 /* File Storage */
-const Storage = multer.diskStorage({
+const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "public/assets");
+        cb(null, "public/assets"); // Set path to save uploaded files
     },
     filename: (req, file, cb) => {
-        cb(null, file.originalname);
+        cb(null, file.originalname); // Set filename to original filename
     },
-    });
+});
+const upload = multer({ storage }); // Create Multer instance for file uploads
+
+/* Mongoose setup */
+const PORT = process.env.PORT || 60001; // Set port to listen on (default: 60001)
+
+mongoose.connect(process.env.MONGO_URL, { // Connect to MongoDB database
+    useNewUrlParser: true, // Use new URL parser
+    useUnifiedTopology: true // Use new server discovery and monitoring engine
+}).then(() => {
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)); // Start listening for requests
+}).catch((error) => console.log(`${error} did not connect`)); // Log error if connection fails
