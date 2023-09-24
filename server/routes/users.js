171672f5db2b1aac664cd907cab37df9260c27cb
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2023-09-24 12:45:50 -0500

    Create user routes in user.js

---
+++ server/routes/users.js
@@ -0,0 +1,18 @@
+import express from "express";
+import{
+    getUsers,
+    getUserConnections,
+    addRemoveConnections,
+} from "../controllers/users.js";
+import { verifyToken } from "../middleware/auth.js";
+
+const router = express.Router(); // Create router
+
+/* Read */
+router.ger("/:id", verifyToken, getUsers); // grab id and find user through that
+router.ger("/:id/connections", verifyToken, getUserConnections); // grab id of connections and find them
+
+/* Update */
+router.patch("/:id/connections", verifyToken, addRemoveConnections); // add or remove connections
+
+export default router; // Export router
