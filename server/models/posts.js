d40f0846868b77b67eb8b4522527663047a837f0
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2023-10-28 23:21:03 -0500

    create post model

---
+++ server/models/posts.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const postSchema = mongoose.Schema({
+    userIds: {
+        type: String,
+        required: true,
+    },
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    location: String, // Optional
+    description: String, // Optional
+    profilePicture: String, // Optional
+    postImage: String, // Optional
+    likes: {
+        type: Map,
+        of: Boolean,
+    },
+    comments: {
+        type: Array,
+        default: [],
+    }
+}, { timestamps: true }); // Add createdAt and updatedAt fields
+
+const Post = mongoose.model("Post", postSchema); // Create Post model
+
+export default Post; // Export Post model to be used in server/controllers/posts.js
+
