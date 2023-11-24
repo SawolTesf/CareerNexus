172fa52300eae08b5d56d5983fbf556231635832
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2023-11-23 20:46:38 -0600

    Define typography settings in theme.js

--- client/src/theme.js
+++ client/src/theme.js
@@ -71,6 +71,34 @@ export const colorTokens = {
                     alt: colorTokens.grey[50]
                 },
             }
-        }
+        },
+        typography: {
+            fontFamily: ["Roboto", "sans-serif"].join(","),
+            fontSize: 12,
+            h1: {
+                fontFamily: ["Roboto", "sans-serif"].join(","),
+                fontSize: 40,
+            },
+            h2: {
+                fontFamily: ["Roboto", "sans-serif"].join(","),
+                fontSize: 32,
+            },
+            h3: {
+                fontFamily: ["Roboto", "sans-serif"].join(","),
+                fontSize: 24,
+            },
+            h4: {
+                fontFamily: ["Roboto", "sans-serif"].join(","),
+                fontSize: 20,
+            },
+            h5: {
+                fontFamily: ["Roboto", "sans-serif"].join(","),
+                fontSize: 16,
+            },
+            h6: {
+                fontFamily: ["Roboto", "sans-serif"].join(","),
+                fontSize: 14,
+            },
+        },
     }
 }
