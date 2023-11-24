1b188b456ec6e1552161ff298ccf7bb55235c457
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2023-11-23 21:25:23 -0600

    Implement theme provider in App component

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,21 @@ import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import HomePage from "scenes/homePage";
 import LoginPage from "scenes/loginPage";
 import ProfilePage from "scenes/profilePage";
+import { useMemo } from "react"; // useMemo is used to prevent the theme from being recreated on every render.
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "theme";
 
 function App() {
+  const mode = useSelector((state) => state.mode); // Get the mode from the Redux store.
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]); // Create the theme using the mode.
+
   return <div className="app">
     <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline /> {/* CssBaseline is used to reset the CSS. */}
+    </ThemeProvider>
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/home" element={<HomePage />} />
