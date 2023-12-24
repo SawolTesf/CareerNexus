42f32ea60c0eda0395680cdb84a890f00ab0142d
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2023-12-23 22:27:12 -0600

    Refactor variable names and add comments for clarity

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -3,14 +3,14 @@ import { Box, IconButton, InputBase, Typography, Select, MenuItem, FormControl, 
 
 // Icons we will be using from https://mui.com/components/material-icons/
 import{
-    Search,
-    Message,
-    Notifications,
-    Help,
-    Menu,
-    LightMode,
-    DarkMode,
-    Close
+    Search as SearchIcon,
+    Message as MessageIcon,
+    Notifications as NotificationsIcon,
+    Help as HelpIcon,
+    Menu as MenuIcon,
+    LightMode as LightModeIcon,
+    DarkMode as DarkModeIcon,
+    Close as CloseIcon,
 } from '@mui/icons-material';
 
 import { useDispatch, useSelector } from 'react-redux'; // useDispatch: Redux hook that is used to dispatch actions. useSelector: Redux hook that is used to access redux state
@@ -18,6 +18,10 @@ import { setMode, setLogout } from "state"; // Actions we will be dispatching
 import { Form, useNavigate } from 'react-router-dom'; // Form: Material UI component that is used to create a form element. useNavigate: React router hook that is used to navigate between pages
 import FlexBetween from 'components/FlexBetween'; // FlexBetween component we created in the components folder
 
+/**
+ * Navbar component that displays the navigation bar at the top of the page.
+ * It includes various elements such as the logo, search bar, user profile, and navigation icons.
+ */
 const Navbar = () => {
     const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false); // State for toggling mobile menu on mobile screens
     const dispatch = useDispatch(); // Redux dispatch hook which is used to dispatch actions. Dispatching actions is how we update the redux state. 
@@ -27,16 +31,16 @@ const Navbar = () => {
     
     // Different themes we will be using
     const theme = useTheme(); // Material UI hook which is used to access theme object. This lets us use the palettes from theme.js
-    const neutralLight = theme.palette.neutral.light; // Neutral light color from theme.js
-    const dark = theme.palette.neutral.dark; // Dark color from theme.js
-    const background = theme.palette.background.default; // Background color from theme.js
-    const primaryLight = theme.palette.primary.light; // Primary light color from theme.js
-    const alt = theme.palette.background.alt; // Alt color from theme.js
+    const neutralLightColor = theme.palette.neutral.light; // Neutral light color from theme.js
+    const darkColor = theme.palette.neutral.dark; // Dark color from theme.js
+    const backgroundColor = theme.palette.background.default; // Background color from theme.js
+    const primaryLightColor = theme.palette.primary.light; // Primary light color from theme.js
+    const altColor = theme.palette.background.alt; // Alt color from theme.js
 
     const fullName = `${user.firstName} ${user.lastName}`; // Full name of user. This is used in the navbar
 
     // Using the FlexBetween component we created in the components folder and passing in some props to style it
-    return <FlexBetween padding="1rem 6%" backgroundColor={alt}>
+    return <FlexBetween padding="1rem 6%" backgroundColor={altColor}>
         <Typography> // Material UI component that is used to create a text element. This is used to create the CareerNexus logo
             fontWeigth="bold"
             fontSize = "clamp(1rem, 2rem, 2.25rem)" // This is a css function that allows us to set a min and max font size. Useful for responsive design
@@ -44,17 +48,17 @@ const Navbar = () => {
             onClick={() => navigate("/home")}
             sx={ { // This is a Material UI prop that allows us to style components using css in js
                 "&:hover": {
-                    color: primaryLight,
+                    color: primaryLightColor,
                     cursor: "pointer",
                 },
             } }
             CareerNexus
         </Typography>
         {isNonMobileScreens && ( 
-            <FlexBetween backgroundColor={neutralLight} borderRadius="9px" gap="3rem" padding="0.1rem 1.5rem">
+            <FlexBetween backgroundColor={neutralLightColor} borderRadius="9px" gap="3rem" padding="0.1rem 1.5rem">
                 <InputBase placeholder='Search' />
                 <IconButton>
-                    <Search />
+                    <SearchIcon />
                 </IconButton>
             </FlexBetween>
         )}
@@ -65,21 +69,21 @@ const Navbar = () => {
             <IconButton onClick={() => dispatch(setMode())}>
                 // Terinary operator that checks if theme is dark or light and renders the appropriate icon
                 {theme.palette.mode === "dark" ? (
-                    <DarkMode sx={ {fontSize: "25px"} }/> // Using the DarkMode component from Material UI if theme is dark mode and passing in some props to style it
+                    <DarkModeIcon sx={ {fontSize: "25px"} }/> // Using the DarkMode component from Material UI if theme is dark mode and passing in some props to style it
                 ): (
-                    <LightMode sx={ { color: dark, fontSize: "25px"} }/> // Using the LightMode component from Material UI if theme is light mode and passing in some props to style it
+                    <LightModeIcon sx={ { color: darkColor, fontSize: "25px"} }/> // Using the LightMode component from Material UI if theme is light mode and passing in some props to style it
                 )}
             </IconButton>
 
             // Material UI component that is used to create a select dropdown
-            <Message sx={ {fontSize: "25px"} }/>
-            <Help sx={ {fontSize: "25px"} }/>
-            <Notifications sx={ {fontSize: "25px"} }/>
+            <MessageIcon sx={ {fontSize: "25px"} }/>
+            <HelpIcon sx={ {fontSize: "25px"} }/>
+            <NotificationsIcon sx={ {fontSize: "25px"} }/>
             // Form component from Material UI that is used to create a form element
             <FormControl variant="standard" value={fullName}> // Passing in some props to style the form. Standard variant is used to create a standard form element
                 // Select component from Material UI that is used to create a select dropdown. We are passing in some props to style the select dropdown such as the background color, width, border radius, padding, and focus color
-                <Select value={ fullName } sx={ { backgroundColor: neutralLight, width: "150px", borderRadius: "0.25rem", p: "0.25rem 1rem", "& .MuiSvgIcon-root": { pr: "0.25rem", width: "3rem" }, "& .MuiSelect-select:focus": { backgroundColor: neutralLight } } } input={<InputBase/>}>
-                    <MenuItem value={fullName}>
+                <Select value={ fullName } sx={ { backgroundColor: neutralLightColor, width: "150px", borderRadius: "0.25rem", p: "0.25rem 1rem", "& .MuiSvgIcon-root": { pr: "0.25rem", width: "3rem" }, "& .MuiSelect-select:focus": { backgroundColor: neutralLightColor } } } input={<InputBase/>}>
+                    <MenuItem value={fullName}> // For adding the user's full name to the select dropdown
                        <Typography>{ fullName }</Typography> 
                     </MenuItem>
                     <MenuItem onClick={() => dispatch(setLogout())}>Logout</MenuItem> // Dispatching the setLogout action when the user clicks on the logout button
