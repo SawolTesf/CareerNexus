9a89d66f7f6cdc4701d48f463e232b448faae9ed
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2024-01-12 21:17:21 -0600

    Add LoginPage component

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,5 +1,20 @@
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import { themeSettings } from "theme";
 const LoginPage = () => {
-    return (<div>LoginPage</div>);
+    const theme = themeSettings();
+    const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+
+    return <Box>
+        <Box width="100%" backgroundColor={theme.palette.background.alt} p="1rem 6%" textAlign="center">
+            <Typography
+                fontWeigth="bold"
+                fontSize="30px" // This is a css function that allows us to set a min and max font size. Useful for responsive design
+                color="primary"
+            >
+                CareerNexus
+            </Typography>
+        </Box>
+    </Box>;
 };
 
 export default LoginPage;
